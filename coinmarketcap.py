1e8ae8bf598c84036bbc93773a5b339ba5f20d53
Javier <javiermoralesestevez@outlook.com>
2022-01-04 20:28:26 +0100

    update

--- coinmarketcap.py
+++ coinmarketcap.py
@@ -1,10 +1,54 @@
 """ Module for requesting data from coinmarketcap.org and parsing it. """
 import json
+import os
+import random
 import time
-
+import datetime as dt
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from requests import Timeout, TooManyRedirects, Session
+
+api_key = 'c78fb3e5-004d-4b44-8613-0f55a60e99c7'
+
+def get_coinmarketcap_latest_api():
+
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+    parameters = {
+        'start': '1',
+        'limit': '10',
+        'price_max': '1',
+        'cryptocurrency_type': 'tokens',
+        'convert': 'EUR',
+    }
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': api_key,
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    try:
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        #
+
+        df_data_id = pd.DataFrame(data['data'], columns=['id', 'symbol', 'date_added'])
+        df_data_quote = pd.DataFrame(data['data'], columns=['quote'])
+        df_quote_json = pd.Series(df_data_quote['quote']).apply(lambda x: x['EUR'])
+        df_data_quote_eur = pd.read_json(df_quote_json.to_json(), orient='index')
+        df = pd.concat([df_data_id, df_data_quote_eur], axis=1)
+        df = pd.DataFrame(df, columns=['id', 'symbol', 'date_added', 'price', 'volume_24h', 'volume_change_24h',
+                   'percent_change_1h', 'percent_change_24h', 'percent_change_7d',
+                   'percent_change_30d', 'percent_change_60d',
+                   'percent_change_90d', 'market_cap', 'market_cap_dominance',
+                   'fully_diluted_market_cap', 'last_updated'])
+
+        # print(df)
+        return df
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e)
 
 
 def _request(target):
@@ -40,13 +84,13 @@ def parseList(broken_html):
         if img.find('1839') != -1:
             # price = row.findAll("td")[3].text.replace('$', '').replace('...', '000').replace('.', ',')
             price_1h_ = row.findAll("td")[4]
-            price_1h = row.findAll("td")[4].text.replace('%', '').replace('.', ',')
+            price_1h = row.findAll("td")[4].text.replace('%', '')#.replace('.', ',')
             if price_1h_.find("span", class_='icon-Caret-up') is not None:
                 price_1h_change = 'up'
             else:
                 price_1h_change = 'down'
             price_24h_ = row.findAll("td")[5]
-            price_24h = row.findAll("td")[5].text.replace('%', '').replace('.', ',')
+            price_24h = row.findAll("td")[5].text.replace('%', '')#.replace('.', ',')
             if price_24h_.find("span", class_='icon-Caret-up') is not None:
                 price_24h_change = 'up'
             else:
@@ -60,11 +104,22 @@ def parseList(broken_html):
             token = requestMarketCap("https://coinmarketcap.com/currencies/" + name.replace(' ', '-'))  # +"/historical-data/?start="+time_last_ticker+"&end="+time_now_ticker)
             url_token, price_token, volume_token = parseToken(token)
 
-            data.append({'name': name, 'symbol': symbol, 'price': price_token, 'volume': volume_token,
+            time_now = dt.datetime.strptime(dt.datetime.now().strftime('%d-%m-%Y %H:%M:%S'), '%d-%m-%Y %H:%M:%S')
+            data = pd.json_normalize({'time': time_now, 'name': name, 'symbol': symbol, 'price': price_token, 'volume': volume_token,
                          'price_1h': price_1h, 'price_1h_change': price_1h_change,
                          'price_24h': price_24h, 'price_24h_change': price_24h_change,
                          'market_cap': market_cap, 'volume_24h': volume_24h, 'url': url_token})
-            time.sleep(1.2)
+
+
+            filename = '{}.csv'.format(name.replace(' ', '-'))
+            if os.path.isfile(filename):
+                df = pd.read_csv(filename)
+                df = df.append(data, ignore_index=True)
+            else:
+                df = pd.DataFrame(data)
+            df.to_csv(filename)
+
+        time.sleep(random.randint(2, 7))
 
     # print(data)
     return data
@@ -75,8 +130,8 @@ def parseToken(broken_html):
     div = content.find('div', class_='content')
     url = div.find('a', class_='cmc-link').get('href').replace('https://bscscan.com/token/', '')
     tbody = soup.find('tbody')
-    price = tbody.findAll("td")[0].text.replace('$', '').replace('.', ',')
-    volume = tbody.findAll("td")[3].find("span").text.replace('$', '').replace(',', '').replace('.', ',')
+    price = tbody.findAll("td")[0].text.replace('$', '')#.replace('.', ',')
+    volume = tbody.findAll("td")[3].find("span").text.replace('$', '').replace(',', '')#.replace('.', ',')
     # print(price)
     # print(volume)
     # print(url)
--- cryptotrader.py
+++ cryptotrader.py
@@ -9,7 +9,6 @@ from requests import Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import coinmarketcap
 
-api_key = 'c78fb3e5-004d-4b44-8613-0f55a60e99c7'
 
 def parse_args():
     parser = argparse.ArgumentParser(description='CCXT Market')
@@ -36,45 +35,6 @@ def get_date_range(number_of_days:int):
     return dt_start, dt_end
 
 
-def get_coinmarketcap_latest_api():
-
-    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-    parameters = {
-        'start': '1',
-        'limit': '10',
-        'price_max': '1',
-        'cryptocurrency_type': 'tokens',
-        'convert': 'EUR',
-    }
-    headers = {
-        'Accepts': 'application/json',
-        'X-CMC_PRO_API_KEY': api_key,
-    }
-
-    session = Session()
-    session.headers.update(headers)
-
-    try:
-        response = session.get(url, params=parameters)
-        data = json.loads(response.text)
-        #
-
-        df_data_id = pd.DataFrame(data['data'], columns=['id', 'symbol', 'date_added'])
-        df_data_quote = pd.DataFrame(data['data'], columns=['quote'])
-        df_quote_json = pd.Series(df_data_quote['quote']).apply(lambda x: x['EUR'])
-        df_data_quote_eur = pd.read_json(df_quote_json.to_json(), orient='index')
-        df = pd.concat([df_data_id, df_data_quote_eur], axis=1)
-        df = pd.DataFrame(df, columns=['id', 'symbol', 'date_added', 'price', 'volume_24h', 'volume_change_24h',
-                   'percent_change_1h', 'percent_change_24h', 'percent_change_7d',
-                   'percent_change_30d', 'percent_change_60d',
-                   'percent_change_90d', 'market_cap', 'market_cap_dominance',
-                   'fully_diluted_market_cap', 'last_updated'])
-
-        # print(df)
-        return df
-    except (ConnectionError, Timeout, TooManyRedirects) as e:
-        print(e)
-
 
 if __name__ == '__main__':
 
@@ -83,13 +43,13 @@ if __name__ == '__main__':
     last_date = dt.datetime.strptime(dt.datetime.now().strftime('%d-%m-%Y %H:%M:%S'), '%d-%m-%Y %H:%M:%S') - dt.timedelta(days=1)
     time_last_ticker = dt.datetime.strptime(last_date.strftime('%d-%m-%Y %H:%M:%S'), '%d-%m-%Y %H:%M:%S')
     time_now_ticker = dt.datetime.strptime(dt.datetime.now().strftime('%d-%m-%Y %H:%M:%S'), '%d-%m-%Y %H:%M:%S')
-    filename = '{}_{}.csv'.format("coinmarketcap", date)
+    # filename = '{}_{}.csv'.format("coinmarketcap", date)
 
     # recuperar las últimas cryptos creadas
     html = coinmarketcap.requestList("https://coinmarketcap.com/es/new/")
     tokens = coinmarketcap.parseList(html)
-    df = pd.DataFrame(tokens)
-    df.to_csv(filename)
-    print(df)
+    # df = pd.DataFrame(tokens)
+    # df.to_csv(filename)
+    # print(df)
 
     print()
