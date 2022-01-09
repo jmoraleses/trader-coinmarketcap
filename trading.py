0fdfa69f71cc5f37ebbfdf0c577fcb80da85c80b
Javier <javiermoralesestevez@outlook.com>
2022-01-09 21:00:55 +0100

    update

--- trading.py
+++ trading.py
@@ -14,8 +14,9 @@ capital = None
 address = None
 private_key = None
 position_open = 0
-position_max = 4 # cantidad total de transacciones permitidas al mismo tiempo
+position_max = 4  # cantidad total de transacciones permitidas al mismo tiempo
 all_tokens = []
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Crypto broker trading')
@@ -47,7 +48,6 @@ def parse_args():
     return parser.parse_args()
 
 
-
 class Broker(object):
 
     def __init__(self, *args, **kwargs):
@@ -71,7 +71,6 @@ class Broker(object):
         self.i = 0
         self.capital = capital
 
-
     def trading(self, df, token, last_operation):
         global position_max
         global position_open
@@ -89,7 +88,7 @@ class Broker(object):
                 self.precio_relativo = self.data.iloc[-1 - self.range]['price'] / self.data.iloc[-1]['price']
                 self.volumen_relativo = self.data.iloc[-1 - self.range]['volume'] / self.data.iloc[-1]['volume']
 
-                if 220000 < self.volume_ini < 3000000: # and self.finish is False:
+                if 220000 < self.volume_ini < 3000000:  # and self.finish is False:
                     if self.precio_relativo <= self.price_relative_range and self.volumen_relativo <= self.volume_relative_range and self.last_operation == "nothing":
 
                         self.coins = self.capital / self.data.iloc[-1]['price']
@@ -112,7 +111,8 @@ class Broker(object):
                         if self.precio_relativo_n >= self.precio_relativo_negativo or self.capital_now >= self.capital_win or self.capital_now <= self.capital_lost:
                             # call sell
                             print("sell")
-                            sell(self.token, self.token_url)
+                            closeTransaction(self.token)
+                            # sell(self.token, self.token_url)
                             return
                             #
         ###
@@ -150,7 +150,7 @@ class Broker(object):
                             last_operation = 'nothing'
 
                         data.append(pd.read_csv("csv/" + file, index_col=0))
-                        processes.append(Process(target=self.trading, args=(data[i], file, last_operation, )))
+                        processes.append(Process(target=self.trading, args=(data[i], file, last_operation,)))
                         i += 1
 
                 print("start")
@@ -175,15 +175,11 @@ def getABI():
     file.close()
 
 
-
 def buy(token_name, token_url):
     global address
     global private_key
     global position_open
     global position_max
-
-
-    sender_address = address
 
     bsc = "https://bsc-dataseed.binance.org/"
     web3 = Web3(Web3.HTTPProvider(bsc))
@@ -193,92 +189,140 @@ def buy(token_name, token_url):
     # getABI() # not used
     # print(PancakeABI)
 
-    # always spend using Wrapped BNB
-    # I guess you want to use other coins to swap you can do that, but for me I used Wrapped BNB
-    spend = web3.toChecksumAddress("0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c") # Wrapped BNB token 0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c
-    # Print out your balances just for checking
-    balance = web3.eth.get_balance(sender_address)
+    # Wrapped BNB
+    spend = web3.toChecksumAddress(
+        "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c")  # Wrapped BNB token 0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c
+    balance_ = web3.eth.get_balance(address)
+    balance = web3.fromWei(balance_, 'ether')
+    # print(balance_)
     print(balance)
-    humanReadable = web3.fromWei(balance, 'ether')
-    print(humanReadable)
-
 
     # Setup the PancakeSwap contract
     contract = web3.eth.contract(address=router_pancake_address, abi=PancakeABI)
-    nonce = web3.eth.get_transaction_count(sender_address)
-    contract_id = web3.toChecksumAddress(token_url) #token_url #Contract id is the new token we are swaping to
-
-    if position_open < position_max:
-        coins = float(balance) / float(position_max - position_open)
-        position_open += 1
+    nonce = web3.eth.get_transaction_count(address)
+    contract_id = web3.toChecksumAddress(token_url)  # token_url #Contract id is the new token we are swaping to
 
     try:
-        # Create the transaction
-        pancakeswap2_txn = contract.functions.swapExactETHForTokens(
-            0, # coins # here setup the minimum destination token you want to have, you can do some math, or you can put a 0 if you don't want to care
-            [spend, contract_id],
-            sender_address,
-            (int(time.time()) + 1000000)
-        ).buildTransaction({
-            'from': sender_address,
-            'value': web3.toWei(coins, 'ether'),  # This is the Token(BNB) amount you want to Swap from
-            'gas': 250000,
-            'gasPrice': web3.toWei('7', 'gwei'),
-            'nonce': nonce,
-        })
 
-        signed_txn = web3.eth.account.sign_transaction(pancakeswap2_txn, private_key=private_key)
-        tx_token = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
-        print('tx: {}'.format(web3.toHex(tx_token)))
+        if position_open < position_max:
+            coins = float(balance - (balance / 10)) / float(position_max - position_open)
+            position_open += 1
 
-        #guardar en csv la fecha, la compra y la cantidad de tokens
-        time_now = dt.datetime.strptime(dt.datetime.now().strftime('%d-%m-%Y %H:%M:%S'), '%d-%m-%Y %H:%M:%S')
-        data = pd.json_normalize({'time': time_now, 'name': token_name, 'operation': 'buy', 'coins': coins, 'token_url': token_url})
-        if os.path.isfile("csv/" + token_name + "_operations.csv"):
-            df = pd.read_csv("csv/" + token_name + "_operations.csv", index_col=0)
-            df = df.append(data, ignore_index=True)
-            df.to_csv("csv/" + token_name + "_operations.csv")
-        else:
-            df = pd.DataFrame(data)
-            df.to_csv("csv/" + token_name + "_operations.csv")
+            # # Create the transaction
+            # pancakeswap2_txn = contract.functions.swapExactETHForTokens(
+            #     0,  # coins or you can put a 0 if you don't want to care
+            #     [spend, contract_id],
+            #     address,
+            #     (int(time.time()) + 1000000)
+            # ).buildTransaction({
+            #     'from': address,
+            #     'value': web3.toWei(coins, 'ether'),  # This is the Token(BNB) amount you want to Swap from
+            #     'gas': 250000,
+            #     'gasPrice': web3.toWei('7', 'gwei'),
+            #     'nonce': nonce,
+            # })
+            #
+            # signed_txn = web3.eth.account.sign_transaction(pancakeswap2_txn, private_key=private_key)
+            # tx_token = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+            # print('Buy {} tx: {}'.format(token_name, web3.toHex(tx_token)))
 
-        return True
+            # Guardar en csv la fecha, la compra y la cantidad de tokens
+            time_now = dt.datetime.strptime(dt.datetime.now().strftime('%d-%m-%Y %H:%M:%S'), '%d-%m-%Y %H:%M:%S')
+            data = pd.json_normalize(
+                {'time': time_now, 'name': token_name, 'operation': 'buy', 'coins': coins, 'token_url': token_url})
+            if os.path.isfile("csv/" + token_name + "_operations.csv"):
+                df = pd.read_csv("csv/" + token_name + "_operations.csv", index_col=0)
+                df = df.append(data, ignore_index=True)
+                df.to_csv("csv/" + token_name + "_operations.csv")
+            else:
+                df = pd.DataFrame(data)
+                df.to_csv("csv/" + token_name + "_operations.csv")
+
+            print('{} Sell {}: {}'.format(time_now, token_name, coins))
+            return True
 
     except ValueError:
-        print("Error al realizar la transacción")
+        time_now = dt.datetime.strptime(dt.datetime.now().strftime('%d-%m-%Y %H:%M:%S'), '%d-%m-%Y %H:%M:%S')
+        print("{} Error al realizar la transacción de compra con {}".format(time_now, token_name))
         return False
 
 
-def sell(token_name, token_url):
-    pass
+def sell(token_name, token_url, coins):
+    global address
+    global private_key
+    global position_open
+
+    bsc = "https://bsc-dataseed.binance.org/"
+    web3 = Web3(Web3.HTTPProvider(bsc))
+    # This is global Pancake V2 Swap router address
+    router_pancake_address = "0x10ED43C718714eb63d5aA57B78B54704E256024E"
+    PancakeABI = open('pancakeABI.txt', 'r').read()
+    # getABI() # not used
+    # print(PancakeABI)
+
+    # Token to BNB
+    spend = web3.toChecksumAddress(token_url) # token url
+
+    # Setup the PancakeSwap contract
+    contract = web3.eth.contract(address=router_pancake_address, abi=PancakeABI)
+    nonce = web3.eth.get_transaction_count(address)
+    contract_id = web3.toChecksumAddress("0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c") # to Wrapped BNB token
+
+    try:
+
+        # # Create the transaction
+        # pancakeswap2_txn = contract.functions.swapExactETHForTokens(
+        #     0,  # coins or you can put a 0 if you don't want to care
+        #     [spend, contract_id],
+        #     address,
+        #     (int(time.time()) + 1000000)
+        # ).buildTransaction({
+        #     'from': address,
+        #     'value': web3.toWei(coins, 'ether'),  # This is the Token(BNB) amount you want to Swap from
+        #     'gas': 250000,
+        #     'gasPrice': web3.toWei('7', 'gwei'),
+        #     'nonce': nonce,
+        # })
+        #
+        # signed_txn = web3.eth.account.sign_transaction(pancakeswap2_txn, private_key=private_key)
+        # tx_token = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+        # print('Sell {} tx: {}'.format(token_name, web3.toHex(tx_token)))
+        time_now = dt.datetime.strptime(dt.datetime.now().strftime('%d-%m-%Y %H:%M:%S'), '%d-%m-%Y %H:%M:%S')
+        print('{} Sell {}: {}'.format(time_now, token_name, coins))
+        position_open -= 1
+        return True
+
+    except ValueError:
+        time_now = dt.datetime.strptime(dt.datetime.now().strftime('%d-%m-%Y %H:%M:%S'), '%d-%m-%Y %H:%M:%S')
+        print("{} Error al realizar la transacción de venta de {}".format(time_now, token_name))
+        return False
 
 
-
-
-def closeTransaction(name_file):
+def closeTransaction(token_name):
     # si existe el archivo de operaciones continuar
-    filename = "csv/" + name_file + "_operations.csv"
+    filename = "csv/" + token_name + "_operations.csv"
     if os.path.isfile(filename):
         df = pd.read_csv(filename, index_col=0)
         if df.iloc[-1]['operation'] == 'buy':
             token_url = df.iloc[-1]['token_url']
             coins = df.iloc[-1]['coins']
             # sell token
-            sell(name_file, token_url)
-            #delete file
-            os.remove("csv/" + name_file + ".csv")
+            sell(token_name, token_url, coins)
+            # delete file
+            os.remove("csv/" + token_name + ".csv")
             # guardar en el archivo transacciones la venta
             time_now = dt.datetime.strptime(dt.datetime.now().strftime('%d-%m-%Y %H:%M:%S'), '%d-%m-%Y %H:%M:%S')
-            data = pd.json_normalize({'time': time_now, 'name': name_file, 'operation': 'sell', 'coins': coins, 'token_url': token_url})
+            data = pd.json_normalize(
+                {'time': time_now, 'name': token_name, 'operation': 'sell', 'coins': coins, 'token_url': token_url})
             df = df.append(data, ignore_index=True)
-            df.to_csv("csv/" + name_file + "_operations.csv")
+            df.to_csv("csv/" + token_name + "_operations.csv")
 
 
 def closeAllTransactions():
     files = os.listdir('csv/')
     for file in files:
         if file.endswith("_operations.csv"):
-            closeTransaction(file.replace('_operations.csv',''))
+            closeTransaction(file.replace('_operations.csv', ''))
 
 
 def find_tokens(broken_html):
@@ -322,7 +366,6 @@ def main():
     # process = Broker()
     # process.run()
     buy("Cake", "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c")
-
 
 
 if __name__ == '__main__':
